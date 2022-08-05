016277f0e92e27bd8784b3109810989c2177254b
ali <aliahmed.pluton@gmail.com>
2022-08-05 12:23:08 +0500

    as

--- src/contract/addresses.js
+++ src/contract/addresses.js
@@ -1,3 +1,3 @@
 
- export const NFT_addr = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
- export const gBPG_addr = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+ export const NFT_addr = "0x0de0Aad8BB8CeA4A0cEC4a90Cca17D1e6084e8D6"
+ export const gBPG_addr = "0x9d9b42B849b58C669668F006a49444E13a751062"
